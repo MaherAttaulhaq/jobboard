5f1fd48ae9f95cbfebb8bf32316f6a5e55753f76
attaulhaq <attaulhaq150@gmail.com>
2026-03-29 12:19:09 +0500

    fix module config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
 };
 
 module.exports = nextConfig;
